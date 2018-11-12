5c9f09223b4f4543c1b950ff8bb2a7970281525c
Braulio96 <brauliobatz@hotmail.com>
2018-11-12 10:01:39 -0600

    Add files via upload

---
+++ TipoProyecto/TipoProyecto/Capa_Datos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Windows.Forms;
+
+
+namespace TipoProyecto
+{
+    public class Capa_Datos
+    {
+        static OdbcConnection conector = null;
+        OdbcDataReader da;
+        OdbcCommand selector;
+        OdbcDataReader lector;
+
+        public static OdbcConnection conex()
+        {
+
+            try
+            {
+                conector = new OdbcConnection("Driver ={ MySQL ODBC 3.51 Driver }; Dsn=sis; UID=root; Pwd=");
+                conector.Open();
+                //MessageBox.Show("Conexion Exitosa");
+            }
+            catch (OdbcException e)
+            {
+                MessageBox.Show("Error al Conectar" + e);
+            }
+            return conector;
+        }
+
+        
+    }
+}
